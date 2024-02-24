0d2bd9c7741a2e48902cd7103cff9c71c91cd6c4
Pushpan Bhaumik <pushpanbhaumik1997@gmail.com>
2024-02-24 21:44:49 +0530

    Modified Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args) {
         int a=12;
-        int b=13;
-        System.out.println(a+b);
+        int b=14;
+        System.out.println(a+b+b);
     }
 }
